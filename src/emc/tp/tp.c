30d29ee569543d826614e81f83805cc45872175d
Rene Hopf <renehopf@mac.com>
2025-07-10 17:23:00 +0200

    cleanup

--- src/emc/tp/tp.c
+++ src/emc/tp/tp.c
@@ -371,9 +371,6 @@ static struct  tp_haldata {
   // Example pin pointers
   hal_u32_t *in;
   hal_u32_t *out;
-  // Example parameters
-//   hal_float_t param_rw;
-//   hal_float_t param_ro;
 } *tp_haldata;
 
 static int makepins(int id) {
@@ -386,10 +383,6 @@ static int makepins(int id) {
     // hal pin examples:
     res += hal_pin_u32_newf(HAL_IN ,&(tp_haldata->in) ,id,"%s.in" ,HAL_PREFIX);
     res += hal_pin_u32_newf(HAL_OUT,&(tp_haldata->out),id,"%s.out",HAL_PREFIX);
-
-    // hal parameter examples:
-    //res += hal_pin_float_newf(HAL_OUT, &tp_haldata->param_rw,id,"%s.param-rw",HAL_PREFIX);
-    //res += hal_pin_float_newf(HAL_IN, &tp_haldata->param_ro,id,"%s.param-ro",HAL_PREFIX);
 
     if (res) goto error;
     rtapi_print("@@@ %s:%s: ok\n",__FILE__,__FUNCTION__);
